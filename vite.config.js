66df8efc6d3a2f2b7970e1c794b0eae346a59881
Ehsan Garmsir <40166739+ehsandc@users.noreply.github.com>
2025-09-16 16:37:53 +0200

    Remove base path from Vite config
    Removed base path configuration from Vite.

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({ base: '/React-Final-Project/', plugins: [react()], })
+export default defineConfig({ plugins: [react()], })
 })
